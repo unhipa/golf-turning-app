6a6043a1aa9d99a59393b26fbe0f2f86928f9b6d
unhipa <ukyodesu123456@gmail.com>
2024-07-27 20:37:54 +0900

    メニューバー変更

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './MenuBar.css';
+import homeIcon from '../assets/HOME-removebg-preview.png';
+import boardIcon from '../assets/投稿-removebg-preview.png';
+import listIcon from '../assets/投稿一覧２-removebg-preview.png';
+import practiceIcon from '../assets/練習アイテム２-removebg-preview.png';
 
 const MenuBar = () => {
   return (
     <div className="menu-bar">
-      <Link to="/">HOME</Link>
-      <Link to="/post-form">掲示板</Link>
-      <Link to="/post-list">投稿一覧</Link>
-      <Link to="/practice-items">練習アイテム</Link>
+      <Link to="/">
+        <img src={homeIcon} alt="HOME" className="menu-icon" />
+      </Link>
+      <Link to="/post-form">
+        <img src={boardIcon} alt="掲示板" className="menu-icon" />
+      </Link>
+      <Link to="/post-list">
+        <img src={listIcon} alt="投稿一覧" className="menu-icon" />
+      </Link>
+      <Link to="/practice-items">
+        <img src={practiceIcon} alt="練習アイテム" className="menu-icon" />
+      </Link>
     </div>
   );
 };
